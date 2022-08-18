72e1f0453b25e27bb5234ac534d458c2674dcc55
Albert Mulia Shintra <chenxeed@gmail.com>
2022-08-18 21:36:34 +0700

    chore: prepare for next API renew

--- src/module/sheet.ts
+++ src/module/sheet.ts
@@ -37,12 +37,18 @@ interface Reservation {
 
 let invitedGuest: Guest
 
+// chenx.phyt@gmail.com
 // const SHEETDB_API_ONLINE_GUEST = 'https://sheetdb.io/api/v1/ieqa1l0tcmfqh'
 // const SHEETDB_API_RESERVATION = 'https://sheetdb.io/api/v1/9z3dmd8qry979'
+// chenxeed.com
 // const SHEETDB_API_ONLINE_GUEST = 'https://sheetdb.io/api/v1/0kkhwj3zpwhlw'
 // const SHEETDB_API_RESERVATION = 'https://sheetdb.io/api/v1/m9ql8dzpeblhv'
+// lianalee@gmail.com
 const SHEETDB_API_ONLINE_GUEST = 'https://sheetdb.io/api/v1/eex5sn7qlj155'
 const SHEETDB_API_RESERVATION = 'https://sheetdb.io/api/v1/jkcuep6ufh4j9'
+// frontendpikto@gmail.com
+// const SHEETDB_API_ONLINE_GUEST = 'https://sheetdb.io/api/v1/2eqwv1kf2tlco'
+// const SHEETDB_API_RESERVATION = 'https://sheetdb.io/api/v1/9rovspjfi91lt'
 
 const fakeReservation: Reservation = {
   invitationName: 'Test Ajah',
